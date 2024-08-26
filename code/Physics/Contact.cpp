f6bc42f964476b4f641bc4a9d76b4ec3190092b6
snakezheng <snakezheng@tencent.com>
2024-08-26 20:04:47 +0800

    Add project files.

---
+++ code/Physics/Contact.cpp
@@ -0,0 +1,40 @@
+//
+//  Contact.cpp
+//
+#include "Contact.h"
+
+/*
+====================================================
+ResolveContact
+====================================================
+*/
+void ResolveContact( contact_t & contact ) {
+	Body * bodyA = contact.bodyA;
+	Body * bodyB = contact.bodyB;
+
+	const float invMassA = bodyA->m_invMass;
+	const float invMassB = bodyB->m_invMass;
+
+	const float elasticityA = bodyA->m_elasticity;
+	const float elasticityB = bodyB->m_elasticity;
+	const float elasticity = elasticityA * elasticityB;
+
+	// Calculate the collision impulse
+	const Vec3 & n = contact.normal;
+	const Vec3 vab = bodyA->m_linearVelocity - bodyB->m_linearVelocity;
+	const float ImpulseJ = -(1.0f + elasticity) * vab.Dot(n)/(invMassA + invMassB);
+	const Vec3 vectorImpulseJ = n*ImpulseJ;
+
+	bodyA->ApplyImpulseLinear(vectorImpulseJ * 1.0f);
+	bodyB->ApplyImpulseLinear(vectorImpulseJ * -1.0f);
+
+	//
+	// Let's also move our colliding objects to just outside of each other
+	//
+	const float tA = bodyA->m_invMass / (bodyA->m_invMass + bodyB->m_invMass);
+	const float tB = bodyB->m_invMass / (bodyA->m_invMass + bodyB->m_invMass);
+
+	const Vec3 ds = contact.ptOnB_WorldSpace - contact.ptOnA_WorldSpace;
+	bodyA->m_position += ds * tA;
+	bodyB->m_position -= ds * tB;
+}
---
+++ code/Scene.cpp
@@ -0,0 +1,101 @@
+//
+//  Scene.cpp
+//
+#include "Scene.h"
+#include "Physics/Contact.h"
+#include "Physics/Intersections.h"
+#include "Physics/Broadphase.h"
+
+/*
+========================================================================================================
+
+Scene
+
+========================================================================================================
+*/
+
+/*
+====================================================
+Scene::~Scene
+====================================================
+*/
+Scene::~Scene() {
+	for ( int i = 0; i < m_bodies.size(); i++ ) {
+		delete m_bodies[ i ].m_shape;
+	}
+	m_bodies.clear();
+}
+
+/*
+====================================================
+Scene::Reset
+====================================================
+*/
+void Scene::Reset() {
+	for ( int i = 0; i < m_bodies.size(); i++ ) {
+		delete m_bodies[ i ].m_shape;
+	}
+	m_bodies.clear();
+
+	Initialize();
+}
+
+/*
+====================================================
+Scene::Initialize
+====================================================
+*/
+void Scene::Initialize() {
+	Body body;
+	body.m_position = Vec3( 0, 0, 10 );
+	body.m_orientation = Quat( 0, 0, 0, 1 );
+	body.m_invMass = 1.0f;
+	body.m_elasticity = 0.5f;
+	body.m_shape = new ShapeSphere( 1.0f );
+	m_bodies.push_back( body );
+
+	body.m_position = Vec3( 0, 0, -1000 );
+	body.m_orientation = Quat( 0, 0, 0, 1 );
+	body.m_invMass = 0.0f;
+	body.m_elasticity = 1.0f;
+	body.m_shape = new ShapeSphere( 1000.0f );
+	m_bodies.push_back( body );
+
+	// TODO: Add code
+}
+
+/*
+====================================================
+Scene::Update
+====================================================
+*/
+void Scene::Update( const float dt_sec ) {
+	// TODO: Add code
+	for (int i = 0; i < m_bodies.size(); i++) {
+		Body *body = &m_bodies[i];
+
+		float mass = body->m_invMass == 0?0:(1.0f/body->m_invMass);
+		Vec3 impulseGravity = Vec3(0,0,-10) * mass * dt_sec;
+		body->ApplyImpulseLinear(impulseGravity);
+	}
+
+	for (int i = 0; i < m_bodies.size(); i++) {
+		for (int j = i + 1; j < m_bodies.size(); j++) {
+			Body * bodyA = &m_bodies[i];
+			Body * bodyB = &m_bodies[j];
+
+			if (0.0f == bodyA->m_invMass && 0.0f == bodyB->m_invMass) {
+				continue;
+			}
+
+			contact_t contact;
+			if (Intersect(bodyA, bodyB, contact)) {
+				ResolveContact(contact);
+			}
+		}
+	}
+
+	for (int i = 0; i < m_bodies.size(); i++) {
+		m_bodies[i].m_position += m_bodies[i].m_linearVelocity * dt_sec;
+	}
+}
